d4142b13c334626fadd06335c3348de7d20fd368
Semyon Goryunov <ficuszet@gmail.com>
2023-11-12 21:24:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# quest_flashing_light
---
+++ quest_flashing_light.ino
@@ -0,0 +1,36 @@
+#define BUTTON_PIN 2
+#define LIGHT_ONE 10
+#define LIGHT_TOW 11
+#define LIGHT_THREE 12
+
+const short lights_pin[3] = {LIGHT_ONE,LIGHT_TOW,LIGHT_THREE};
+const short shoots[12] = {LIGHT_ONE,LIGHT_TOW,LIGHT_ONE,
+                          LIGHT_THREE,LIGHT_ONE,LIGHT_TOW,
+                          LIGHT_ONE,LIGHT_THREE,LIGHT_THREE,LIGHT_ONE};
+bool isStartLight = false;
+
+void setup() {
+
+  //start serial connection
+//  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  for(short pin:lights_pin){
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+  }
+}
+
+void loop() {
+  int sensorVal = digitalRead(BUTTON_PIN);
+//  if(sensorVal == LOW && !isStartLight) isStartLight = true;
+  if(sensorVal == LOW){
+    for(short pin:shoots){
+      digitalWrite(pin, LOW);
+      delay(1000);
+      digitalWrite(pin, HIGH);
+      delay(1000);
+    }
+//    isStartLight = false;
+//      for(short pin:lights_pin) digitalWrite(pin, LOW);
+  };
+}
